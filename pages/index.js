0086fd459440e16b5d7a26178b5ad8f858376c4a
Lucas Travagin <lucas_travagin1112@hotmail.com>
2019-07-22 00:21:38 -0300

    Create Component About

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-const Index = () => {
-    return (
-        <h1>I am Index Page</h1>
-    )
+import React from 'react'
+
+// const Index = () => {
+//     return (
+//         <h1>I am Index Page</h1>
+//     )
+// }
+
+class Index extends React.Component {
+    render() {
+        return (
+            <h1>I am Index Page</h1>
+        )
+    }
 }
 
 export default Index;
