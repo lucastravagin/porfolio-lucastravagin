2de5bc5f8f055e966d89a125e15e513597407146
Lucas Travagin <lucas_travagin1112@hotmail.com>
2019-07-22 02:34:00 -0300

    Simpla Navigation e More Pages

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,17 @@ import React from 'react'
 // }
 
 class Index extends React.Component {
+    
     render() {
         return (
-            <h1>I am Index Page</h1>
+            <div>
+                <h1>I am Index Page</h1>
+                <a href="/"> Home </a>
+                <a href="/blogs"> Blog </a>
+                <a href="/portfolios"> Portfolio </a>
+                <a href="/about"> Sobre </a>
+                <a href="/cv"> CV </a>
+            </div>
         )
     }
 }
